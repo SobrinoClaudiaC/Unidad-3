f74627cca5bcc58e977b2f508d5bb378fa00b7f4
Claud143 <clasbrcstro@gmail.com>
2024-08-29 21:12:28 -0600

    arraysmodificados

---
+++ Sem9/s42/assets/js/script.js
@@ -0,0 +1,42 @@
+// Búsqueda en arreglos
+let letras = ["a", "b", "c", "a", "d", "e", "f", "a", "g", "h"];
+console.log(letras);
+
+console.log("indexOf");
+
+let indice = letras.indexOf("a");
+console.log(indice);
+console.log(letras.indexOf("d"));
+
+console.log(letras.indexOf("a",4));
+
+console.log(letras.indexOf("x"));
+
+console.log("LastIndexOf");
+console.log(letras.lastIndexOf("a"));
+console.log(letras.lastIndexOf("a", 1));
+
+//includes (elemento, desde)
+console.log("includes");
+console.log(letras.includes("a"));
+console.log(letras.includes("c", 3));
+
+//find index
+let nums = [34, 65, 23, 45, 96, 143, 4, 56];
+console.log(nums);
+
+let coincidencia = nums.find((num) => num > 60);
+let coincidenciaIndex = nums.findIndex((num) => num > 60);
+let filtrado = nums.filter((num) => num > 60);
+
+
+console.log(coincidencia);
+console.log(coincidenciaIndex);
+console.log(filtrado);
+
+
+
+
+
+
+
