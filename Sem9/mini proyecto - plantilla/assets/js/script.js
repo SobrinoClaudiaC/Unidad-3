cf78f88a629d4daeb5026493a68ddccd4126aae3
Claud143 <clasbrcstro@gmail.com>
2024-08-29 19:43:41 -0600

    miniproyectoterminado

--- Sem9/mini proyecto - plantilla/assets/js/script.js
+++ Sem9/mini proyecto - plantilla/assets/js/script.js
@@ -1,4 +1,4 @@
-const cars = ["bmw", "volvo", "saab", "ford", "fiat", "audi"];
+let cars = ["bmw", "volvo", "saab", "ford", "fiat", "audi", "ferrari"];
 
 let btnPush = document.getElementById("btn_push");
 
@@ -20,3 +20,103 @@ document.getElementById("demo").innerHTML = text;
 }
 
 mostrarImagenes();
+
+
+let btnPop = document.getElementById("btn_pop");
+
+btnPop.onclick = funcionPop;
+
+function funcionPop(){
+    if (cars.length > 0){
+    cars.pop();
+    mostrarImagenes();
+    } else {
+        alert("No se puede eliminar más elementos ya que el arreglo esta vacío");
+    }
+}
+
+function mostrarImagenes(){
+let text = "";
+
+for (let car of cars){
+    text += `<img src="./assets/img/${car}.png" alt="${car}" width="100"/>`;
+}
+
+document.getElementById("demo").innerHTML = text;
+}
+
+mostrarImagenes();
+
+
+let btnShift = document.getElementById("btn_shift");
+
+btnShift.onclick = funcionShift;
+
+function funcionShift(){
+    cars.shift("");
+    mostrarImagenes();
+}
+
+function mostrarImagenes(){
+let text = "";
+
+for (let car of cars){
+    text += `<img src="./assets/img/${car}.png" alt="${car}" width="100"/>`;
+}
+
+document.getElementById("demo").innerHTML = text;
+}
+
+mostrarImagenes();
+
+
+let btnUnshift = document.getElementById("btn_unshift");
+
+btnUnshift.onclick = funcionUnshift;
+
+function funcionUnshift(){
+    cars.unshift("lexus");
+    mostrarImagenes();
+}
+
+function mostrarImagenes(){
+let text = "";
+
+for (let car of cars){
+    text += `<img src="./assets/img/${car}.png" alt="${car}" width="100"/>`;
+}
+
+document.getElementById("demo").innerHTML = text;
+}
+
+mostrarImagenes();
+
+
+let btnSlice = document.getElementById("btn_slice");
+
+btnSlice.onclick = funcionSlice;
+
+function funcionSlice(){
+    let begin = document.getElementById("begin").value;
+    let end = document.getElementById("end").value;
+
+    if(begin != "" && end != ""){
+        cars = cars.slice(begin, end);
+        mostrarImagenes();
+    } else {
+        alert("Los campos no pueden estar vacíos");
+    }  
+}
+
+function mostrarImagenes(){
+let text = "";
+
+for (let car of cars){
+    text += `<img src="./assets/img/${car}.png" alt="${car}" width="100"/>`;
+}
+
+document.getElementById("demo").innerHTML = text;
+}
+
+mostrarImagenes();
+
