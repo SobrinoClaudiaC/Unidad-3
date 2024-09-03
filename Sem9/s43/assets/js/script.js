a4acb4a192e55c9f91c63ee2c468b3fc5668b6c0
Claud143 <clasbrcstro@gmail.com>
2024-09-02 20:18:52 -0600

    ordenamientonumerico

---
+++ Sem9/s43/assets/js/script.js
@@ -0,0 +1,54 @@
+//Modificación de arrays
+//.map(función)
+
+let nums = [1,2,3,4,5];
+console.log(nums);
+
+let numspordos = nums.map((n)=>n*2);
+console.log(numspordos);
+console.log(nums);
+
+// 3 -> Del arreglo de strings, crear un nuevo arreglo con la siguiente pauta: Si la palabra tiene más de 7 letras se pondrá "palabra larga", en caso contrario se pondrá "palabra corta"
+let palabras = [
+    "hola",
+    "computadora",
+    "casa",
+    "programacion",
+    "javascript",
+    "ciclo",
+  ];
+console.log(palabras);
+
+let palabrasSize = palabras.map((p)=>{
+    if (p.length>7){
+        return "palabra larga";
+    }
+    return "palabra corta";
+});
+console.log(palabrasSize);
+  // ["palabra corta", "palabra larga", "palabra corta", "palabra larga", "palabra larga", "palabra corta"]
+const numes = [1,2,3,4,5];
+console.log(numes);
+
+let suma = numes.reduce((a,b)=>a+b,0);
+console.log(suma);
+
+let multiplicacion = numes.reduce((a,b)=>a*b,1);
+console.log(multiplicacion);
+
+let puntos = [11,2,1,21,23,10];
+console.log(puntos);
+
+puntos.sort();
+console.log("ordenamiento por puntos");
+console.log(puntos);
+
+puntos.sort((a,b)=>a-b);
+console.log("ordenamiento númerico estricto");
+console.log(puntos);
+
+
+
+
+
+
