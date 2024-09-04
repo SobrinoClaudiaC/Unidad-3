b6fac5b55ec0940f64134e29fa7c7f689a10d0e3
Claud143 <clasbrcstro@gmail.com>
2024-09-03 20:57:44 -0600

    objetos y estructura de datos

---
+++ Sem10/s46/assets/js/script.js
@@ -0,0 +1,26 @@
+//Objetos literales
+const persona = {
+    nombre: "Pedro",
+    apellido: "Perez",
+    edad: 25,
+    hobbies: ["ver películas, dibujar"]
+};
+console.log(persona);
+//Acceder o modificar un objeto
+console.log(persona["nombre"]);
+console.log(persona.edad);
+console.log(persona["apellido"]);
+
+persona.correo = "uncorreo@correo.com",
+console.log(persona);
+
+persona.correo = "pedroperez@hotmail.com";
+console.log(persona);
+//Agregar un valor
+persona.trabajo = "informático";
+console.log(persona);
+
+
+
+
+
