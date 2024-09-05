30302b41aabfebb8bc19c2644e1f991138a92da9
Claud143 <clasbrcstro@gmail.com>
2024-09-04 21:35:53 -0600

    JQuery

--- Sem10/s49/suubsesion49/assets/js/script2.js
+++ Sem10/s49/suubsesion49/assets/js/script2.js
@@ -1,9 +1,21 @@
 $(document).ready(function(){
+//Con JS
     document.querySelector("#title").innerHTML = "DOM con JS";
-
+//Con JQuery
     $("#title").html("DOM con JQuery");
 //Seleccionar por clase
     $(".demo").html("Clase seleccionada")
-    //Solo usar el primero de una clase
+//Solo usar el primero de una clase
     $(".demo").first().html("Primer elemento de la clase")
+//Seleccionar elemento y cambiar atributo
+$("img").attr
+("src", "https://cdn.britannica.com/26/162626-050-3534626F/Koala.jpg");
+
+$("img").attr
+("title", "Un koala");
+
+$("#btn").click
+(function () {
+    $("#resultado").toggle();
+});
 });
